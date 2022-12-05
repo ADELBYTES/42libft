4a9c283c18a572c7c3835ac85c72684997ae8687
Mohammed Adel <mohnour.pro@gmail.com>
2022-12-05 11:51:32 +0400

    first

---
+++ ft_calloc.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+void *ft_calloc(size_t num, size_t size){
+
+void *p;
+size_t i;
+
+
+p = malloc(num * size);
+if (!p)
+    return (NULL);
+while (i < num * size){
+
+((unsigned char *)p)[i] = '0';
+++i;
+
+}
+return (p);
+
+
+}
